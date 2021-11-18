c1806d29859acbe49dd1ed87bfb4d5a6c5999127
Andreysha17 <90186083+Andreysha17@users.noreply.github.com>
2021-11-18 12:53:52 +0200

    lab6.py

--- lab6.py
+++ lab6.py
@@ -1,10 +1,31 @@
-- 👋 Hi, I’m @Andreysha17
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+import requests
 
-<!---
-Andreysha17/Andreysha17 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+url=input('Enter the URL:\n')
+
+def parse(url):
+    r=requests.get(url)
+    soup=BeautifulSoup(r.content, 'html.parser') 
+    return soup
+
+page=parse(url)
+content=page.find('div', 'content-article')
+
+def inside():
+    r=requests.get(url)
+    soup=BeautifulSoup(r.content, 'html.parser')
+    i=0
+    for link in soup.find_all('a', href=True):
+        i+=1  
+        text=page.find('div', 'content-article')
+        img=len([tag.name for tag in page.find_all('img')])
+        links=len([tag.name for tag in page.find_all('link')])
+        tags=len([tag.name for tag in page.find_all()])
+
+    print('У тексті:')
+    print('Зображення:', img)
+    print('Теги:', tags)
+    print('Посилання:', links)
+print(inside())
